82c8140305dc4268dc2db33820cb35ec5d80e6b9
Leonardo Facundes <leonardo.fm00@gmail.com>
2019-11-27 02:03:03 -0300

    reescrevendo

--- cliente.py
+++ cliente.py
@@ -1,55 +1,53 @@
-from socket import socket, AF_INET, SOCK_STREAM
-from threading import Thread
+# telnet program example
+import socket, select, string, sys
 
-class Send:
-    def __init__(self):
-        self.__msg = ''
-        self.new = True
-        self.con = None
-    
-    def put(self, msg):
-        self.__msg = msg
-        if self.con != None:
-            self.con.send(str.encode(self.__msg))
-    
-    def get(self):
-        return self.__msg
-    
-    def loop(self):
-        return self.new
+def prompt() :
+	sys.stdout.write('<You> ')
+	sys.stdout.flush()
 
-def esperar(tcp, send, host = 'localhost', port = 5000):
-    destino = (host, port)
-    tcp.connect(destino)
-
-    while send.loop():
-        print('Conectado a ', host, '.')
-        send.con = tcp
-
-        while send.loop():
-            msg = tcp.recv(1024)
-            if not msg: break
-            print(str(msg, 'utf-8'))
-
-if __name__ == '__main__':
-    print('Digite o nome ou IP do servidor (localhost): ')
-    host = input()
-
-    if host == '':
-        host = '127.0.0.1'
-    
-    tcp = socket(AF_INET, SOCK_STREAM)
-    send = Send()
-    
-    processo = Thread(target=esperar, args=(tcp, send, host))
-    processo.start()
-    print('')
-
-    msg = input()
-    while True:
-        send.put(msg)
-        msg = input()
-
-    processo.join()
-    tcp.close()
-    exit()
+#main function
+if __name__ == "__main__":
+	
+	if(len(sys.argv) < 3) :
+		print 'Usage : python telnet.py hostname port'
+		sys.exit()
+	
+	host = sys.argv[1]
+	port = int(sys.argv[2])
+	
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.settimeout(2)
+	
+	# connect to remote host
+	try :
+		s.connect((host, port))
+	except :
+		print 'Unable to connect'
+		sys.exit()
+	
+	print 'Connected to remote host. Start sending messages'
+	prompt()
+	
+	while 1:
+		socket_list = [sys.stdin, s]
+		
+		# Get the list sockets which are readable
+		read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+		
+		for sock in read_sockets:
+			#incoming message from remote server
+			if sock == s:
+				data = sock.recv(4096)
+				if not data :
+					print '\nDisconnected from chat server'
+					sys.exit()
+				else :
+					#print data
+					sys.stdout.write(data)
+					prompt()
+			
+			#user entered a message
+			else :
+				msg = sys.stdin.readline()
+				s.send(msg)
+				prompt()
